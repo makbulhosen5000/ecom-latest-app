cab4a9deed27b540ee28f3e4ce66f7af62a54a77
makbulhosen5000 <makbulhosen5000@gmail.com>
2025-03-18 22:07:47 +0600

    no commit

--- database/migrations/2025_03_10_080414_create_customer_profiles_table.php
+++ database/migrations/2025_03_10_080414_create_customer_profiles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customer_profiles', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string('cus_name',100);
             $table->string('cus_address',500);
